450bb86c4a86c03c098c6dd5c8fb8f4635d8a366
Lily Martin <119932838+LilyMatilde@users.noreply.github.com>
2024-06-07 10:35:23 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', function(){
+    alert('Thank you for letting us know how awesome we are! See you soon in the races!');
+});
