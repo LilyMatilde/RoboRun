5eaf2a1e49f4ef3d9e6a8003343021170989c107
Lily Martin <119932838+LilyMatilde@users.noreply.github.com>
2024-06-07 14:23:24 +0100

    Updates to varios parts

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let submitButton = document.getElementById('submitButton');
+let signUp = document.getElementById('signUp');
 
-submitButton.addEventListener('click', function(){
+
+   submitButton.addEventListener('click', function(){
     alert('Thank you for letting us know how awesome we are! See you soon in the races!');
+}); 
+
+
+
+signUp.addEventListener('click', function(){
+prompt('Tell us your email and we will contact you there for more form filling information!');
+
 });
