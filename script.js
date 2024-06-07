fdf0154905e708e739a3884ba6b002786ea3eb9b
Lily Martin <119932838+LilyMatilde@users.noreply.github.com>
2024-06-07 14:35:36 +0100

    code fixed functions work
    How to check if an element exists with vanilla JavaScript
    
    // Try to get the element
    let elem = document.querySelector('#nope');
    
    // If it exists, do stuff...
    if (elem) {
    	// Do code!
    }

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let submitButton = document.getElementById('submitButton');
 let signUp = document.getElementById('signUp');
 
-
+if(submitButton){
    submitButton.addEventListener('click', function(){
     alert('Thank you for letting us know how awesome we are! See you soon in the races!');
 }); 
-
-
-
+} else if(signUp){
 signUp.addEventListener('click', function(){
 prompt('Tell us your email and we will contact you there for more form filling information!');
 
 });
+
+}
+
+
